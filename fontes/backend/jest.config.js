5e50d1eb09b04a33ef1ac3473ead47cca0b0edc6
SabrinaNobuntu <sabrina@nobuntu.com.br>
2025-09-24 13:30:14 -0300

    feat: Adiciona package.json na raiz para testes de CI 3

--- fontes/backend/jest.config.js
+++ fontes/backend/jest.config.js
@@ -7,20 +7,20 @@ module.exports = {
       displayName: 'backend',
       preset: 'ts-jest',
       testEnvironment: 'node',
+      // Define a raiz do projeto backend
+      rootDir: '<rootDir>/fontes/backend',
       // Aponta para os arquivos de teste do backend
-      testMatch: ['<rootDir>/fontes/backend/src/tests/**/*.spec.ts'],
-      // Mapeia o caminho para o Jest encontrar o código-fonte do backend
-      rootDir: '<rootDir>',
+      testMatch: ['<rootDir>/src/tests/**/*.spec.ts'],
     },
     // Configuração para o frontend
     {
       displayName: 'frontend',
       preset: 'ts-jest',
       testEnvironment: 'jsdom',
+      // Define a raiz do projeto frontend
+      rootDir: '<rootDir>/fontes/frontend',
       // Aponta para os arquivos de teste do frontend
-      testMatch: ['<rootDir>/fontes/frontend/src/tests/**/*.spec.ts'],
-      // Mapeia o caminho para o Jest encontrar o código-fonte do frontend
-      rootDir: '<rootDir>',
+      testMatch: ['<rootDir>/src/tests/**/*.spec.ts'],
     },
   ],
 };
